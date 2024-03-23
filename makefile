60e21a4f57f74b20e918eebcb3b3556b1bbacf00
bdalfavero <bdalfavero@gmail.com>
2024-03-23 10:48:13 -0400

    Started on the parallel multiply. Next we need to collect chunks of the product from the nodes, and put them into the main matrix.

--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 all: matmul matmul_test
 
-objects=src/main.o src/matrix.o src/block_matrix.o src/get_walltime.o
+objects=src/main.o src/matrix.o src/block_matrix.o src/get_walltime.o src/mpi_matrix_multiply.o
 
 matmul_test: src/matmul_test.o src/matrix.o src/block_matrix.o
 	gcc src/matmul_test.o src/matrix.o src/block_matrix.o -o matmul_test
@@ -9,7 +9,7 @@ src/matmul_test.o: src/matmul_test.c src/block_matrix.o include/block_matrix.h
 	gcc -c src/matmul_test.c -o src/matmul_test.o
 
 matmul: $(objects)
-	gcc $(objects) -o matmul
+	gcc $(objects) -o matmul -lm
 
 src/main.o: src/main.c
 	gcc -c src/main.c -o src/main.o
@@ -23,3 +23,5 @@ src/block_matrix.o: src/block_matrix.c include/block_matrix.h
 src/get_walltime.o: src/get_walltime.c 
 	gcc -c src/get_walltime.c -o src/get_walltime.o
 
+src/mpi_matrix_multiply.o: src/mpi_matrix_multiply.c include/mpi_matrix_multiply.h
+	gcc -c src/mpi_matrix_multiply.c -o src/mpi_matrix_multiply.o -lm
---
+++ src/mpi_matrix_multiply.c
@@ -0,0 +1,40 @@
+#include <mpi.h>
+#include <math.h>
+#include "../include/block_matrix.h"
+
+void mpi_matrix_multiply(block_matrix_t a, block_matrix_t b, block_matrix_t *c, int rank, int world_size){
+	/* Parallelization strategy: 
+	 * Split A into groups of rows. Mutliply this by all of B on each node,
+	 * and this forms a group of rows of C. Send all rows to node zero to concatenate the matrix together.
+	 */
+	int rows_per_rank, this_node_row_min, this_node_row_max;
+	rows_per_rank = (int)ceil((double)a.rows / (double)world_size);
+	this_node_row_min = rank * rows_per_rank;
+	this_node_row_max = (rank + 1) * rows_per_rank - 1;
+	if (this_node_row_max > a.rows) this_node_row_max = a.rows;
+	
+	// Store a chunk of A in a separate matrix.
+	block_matrix_t chunk_a;
+	chunk_a.rows = this_node_row_max - this_node_row_min + 1;
+	chunk_a.cols = a.cols;
+	block_matrix_alloc(&chunk_a);
+	for (int i = this_node_row_min; i <= this_node_row_max; i++) {
+		for (int j = 0; j < chunk_a.cols; j++) {
+			matrix_set((&chunk_a), i, j, matrix_get(a, i, j));
+		}
+	}
+
+	// Make a chunk of C to store the partial multiplication result.
+	block_matrix_t chunk_c;
+	chunk_c.rows = chunk_a.rows;
+	chunk_c.cols = b.cols;
+	block_matrix_alloc(&chunk_c);
+	// Multiply the chunk of A by all of B.
+	block_matrix_multiply(chunk_a, b, &chunk_c);
+
+	// Send all chunks to rank 0.
+	// Assign these chunks to the elements of C.
+
+	block_matrix_free(&chunk_a);
+	block_matrix_free(&chunk_c);
+}
