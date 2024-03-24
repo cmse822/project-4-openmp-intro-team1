c3b2dabff90bed6ba33ed41fd1f51fcda50d69bf
Xiaotian <156108153+xiaothua@users.noreply.github.com>
2024-03-24 17:02:45 -0400

    add #include <stdlib.h> for "malloc"

--- src/mpi_matrix_multiply.c
+++ src/mpi_matrix_multiply.c
@@ -1,5 +1,6 @@
 #include <mpi.h>
 #include <math.h>
+#include <stdlib.h>
 #include "../include/block_matrix.h"
 
 void mpi_matrix_multiply(block_matrix_t a, block_matrix_t b, block_matrix_t *c, int rank, int world_size){
